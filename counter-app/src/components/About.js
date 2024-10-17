f76c17fb8da44f5f661579e0d5b1ba756fde3fe4
Dilshodbek Isokjonov <techprouz@gmail.com>
2024-10-17 21:53:12 +0500

    About component

---
+++ counter-app/src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+  return (
+    <section>
+      <h2>About</h2>
+      <p>This application demonstrates basic React functionalities including state management and routing.</p>
+    </section>
+  );
+}
+
+export default About;
