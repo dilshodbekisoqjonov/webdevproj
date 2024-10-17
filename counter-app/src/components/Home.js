a458bae30af44b9f1f049240cde9c54a90391af9
Dilshodbek Isokjonov <techprouz@gmail.com>
2024-10-17 22:01:18 +0500

    Home Component

---
+++ counter-app/src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <section>
+      <header>
+        <h2>Welcome to the Counter App</h2>
+      </header>
+      <p>This application helps you increment, decrement, and reset a counter.</p>
+    </section>
+  );
+}
+
+export default Home;
